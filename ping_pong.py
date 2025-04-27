157fbbb63d71205182cd4b32e1e74e05c8e17958
Alexey26010 <alexis26102010@gmail.com>
2025-04-27 14:27:40 +0300

    Add files via upload

--- ping_pong.py
+++ ping_pong.py
@@ -26,15 +26,18 @@ class Player(GameSprite):
             self.rect.y -= self.speed
         if keys[K_DOWN] and self.rect.y < 520:
             self.rect.y += self.speed
-player_1 = Player('free-icon-table-tennis-racket-4840943.png', 5, 270, 10, 75, 85)
-player_2 = Player('free-icon-table-tennis-racket-4840943.png', 825, 270, 10, 75, 85)
+player_1 = Player('square.png', 40, 270, 10, 15, 95)
+player_2 = Player('square.png', 825, 270, 10, 15, 95)
+ball = GameSprite('free-icon-ball-802340.png', 425, 300, 15, 50, 50)
 font.init()
 font2 = font.SysFont('Arial Black', 80)
 lose1 = font2.render('Player 1 LOSE', True, (180, 0, 0))
 lose2 = font2.render('Player 2 LOSE', True, (180, 0, 0))
-font1 = font.SysFont('Arial Black', 36)
 game = True
 finish = False
+l = [-3, 3]
+speed_x = l[randint(0, 1)]
+speed_y = l[randint(0, 1)]
 while game:
     for e in event.get():
         if e.type == QUIT:
@@ -45,9 +48,30 @@ while game:
         player_1.update_l()
         player_2.reset()
         player_2.update_r()
+        ball.reset()
+        if finish != True:
+            ball.rect.x += speed_x
+            ball.rect.y += speed_y
+        if ball.rect.y > 600 - 50 or ball.rect.y < 0:
+            speed_y *= -1
+        if sprite.collide_rect(player_1, ball) or sprite.collide_rect(player_2, ball):
+            speed_x *= -1
+        if ball.rect.x < 0:
+            finish = True
+            window.blit(lose1, (200, 200))
+        if ball.rect.x > 900:
+            finish = True
+            window.blit(lose2, (200, 200))
         display.update()
     else:
-        finish=False
-
+        finish = False
+        ball.rect.x = 425
+        ball.rect.y = 300
+        player_1.rect.x = 40
+        player_1.rect.y = 270
+        player_2.rect.x = 825
+        player_2.rect.y = 270
+        speed_x = l[randint(0, 1)]
+        speed_y = l[randint(0, 1)]
         time.delay(3000)
     time.delay(50)
